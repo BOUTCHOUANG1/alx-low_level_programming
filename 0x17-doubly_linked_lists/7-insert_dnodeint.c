7c5df5b5fe9641a483c0f4a4a0fadf3c8a234bdc
BOUTCHOUANG1 <101360639+BOUTCHOUANG1@users.noreply.github.com>
2022-07-08 00:49:46 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,52 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at specific index
- * @h: head of linked list
- * @idx: index of new node
- * @n: new node value
- * Return: inserted node
+ * insert_dnodeint_at_index - Function that adds a node at a given position
+ * @h: Head of the linked list
+ * @idx: Index of the list where the new node should be added
+ * @n: Number of the variable of the linked list
+ * Return: The address of the new node or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current;
-	dlistint_t *new;
+	dlistint_t *new = NULL, *tmp = NULL;
+	unsigned int i;
 
 	if (h == NULL)
-		return (0);
-
-	current = *h;
-
-	while (idx != 0)
+		return (NULL);
+	if (*h == NULL)
 	{
-		current = current->next;
-		idx--;
-		if (current == NULL)
-			return (NULL);
+		new = add_dnodeint(h, n);
+		return (new);
 	}
+	tmp = *h;
+	for (i = 0; tmp->next; i++)
+		tmp = tmp->next;
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	else if (idx == i + 1)
+	{
+		new = add_dnodeint_end(h, n);
+		return (new);
+	}
+	else if (idx > i)
+		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
-
 	if (new == NULL)
-	{
-		free(new);
 		return (NULL);
-	}
-
 	new->n = n;
-	new->next = current;
-	new->prev = current->prev;
-	if (current->prev != NULL)
-		current->prev->next = new;
+	new->prev = NULL;
+	new->next = NULL;
+	for (; i != idx - 1; i--)
+		tmp = tmp->prev;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+	new->prev = tmp;
 
-	/*TODO: Handle special case when idx is 0 and last index*/
-
-	return (current);
+	return (new);
 }
